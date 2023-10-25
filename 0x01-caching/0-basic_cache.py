0f665a8f35fd1e5334768d4fab61443fabe32f7d
Becky1703 <oluwabukunmia@gmail.com>
2023-10-25 12:12:46 +0000

    basic cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Caching"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Class inherits from BaseCaching"""
+    def put(self, key, item):
+        """Fnction to put data into a caching system"""
+        if key or item is None:
+            pass
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Fnction to retrieve the data stored in the cached system"""
+        return self.cache_data.get(key, None)
