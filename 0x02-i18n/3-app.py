9cca548c9ae0e0bab1c0ab01b79975e35f839f61
Becky1703 <oluwabukunmia@gmail.com>
2023-10-31 13:14:21 +0000

    error fix

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -4,7 +4,7 @@ from flask_babel import Babel
 from flask import Flask, render_template, request
 
 
-class Config(object):
+class Config:
     """Babel Configuration for the flask application"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
@@ -13,7 +13,7 @@ class Config(object):
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# app.url_map.strict_slashes = False
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
