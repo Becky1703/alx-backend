54e07e91ff4bcf743aaf11b10b546c3bc5f828ad
Becky1703 <oluwabukunmia@gmail.com>
2023-10-31 12:58:21 +0000

    translations

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Flask application"""
+from flask_babel import Babel
+from flask import Flask, render_template, request
+
+
+class Config(object):
+    """Babel Configuration for the flask application"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localselector
+def get_locale() -> str:
+    """function selects the best match for the supported language"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def get_index() -> str:
+    """Defines and returns the home route"""
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
