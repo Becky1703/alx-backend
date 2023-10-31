1e9313341df33e3b8ae21876f1be91b3661758e7
Becky1703 <oluwabukunmia@gmail.com>
2023-10-31 11:14:53 +0000

    made corrections

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 """Flask application"""
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def get_index() -> str:
     """Defines the home route"""
-    return render_template('index.html')
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
