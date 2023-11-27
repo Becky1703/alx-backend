779cb5293c8b6948acb5b6d3462604cfdd67bc0b
Becky1703 <oluwabukunmia@gmail.com>
2023-11-27 21:10:29 +0000

    queue

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import {createQueue } from 'kue';
+
+const queue = createQueue({name: 'push_notification_code'});
+
+const job = queue.create('push_notification_code', {
+  phoneNumber: '0905432156',
+  message: 'Account registered',
+});
+
+job
+  .on('enqueue', () => {
+    console.log('Notification job created:', job.id);
+  })
+  .on('complete', () => {
+    console.log('Notification job failed');
+  });
+job.save();
